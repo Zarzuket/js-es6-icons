dce2b33dae7b657d259f28d8e8917a714d5c1e6a
Zarzuket <davide.bajo@gmail.com>
2021-09-13 17:50:13 +0200

    add colored icons + select

--- js/main.js
+++ js/main.js
@@ -108,19 +108,84 @@ const icons = [
 	  category: "animal"
 	},
   ];
+  
+const colors ={
+    food: "red",
+    animal: "yellow",
+    beverage: "orange"
+}
 
+
+// FUNZIONI
+// accetta array icone + il div dove stampare le icone
+const printIcons = (arr, container) => {
+    container.innerHTML = "";
+    arr.forEach(
+        (element) => {
+            const {name, family, prefix, color} = element;
+    
+            container.innerHTML += `
+            <div class="icon">
+                <i class="${family} ${prefix}${name}" style="color:${color}"></i>
+                <div class="icon-name">${name}</div>
+            </div>
+            `;
+        
+        }
+    );
+}
+
+// Programma principale
 const containerIcons = document.getElementById("icons");
 
-icons.forEach(
-    (element) => {
-        const {name, family, prefix} = element;
 
-        containerIcons.innerHTML += `
-        <div class="icon">
-            <i class="${family} ${prefix}${name}"></i>
-            <div class="icon-name">${name}</div>
-        </div>
-        `;
-    
+//icone colorate per tipo
+const colorIcons = icons.map(
+    (element) => {
+        return {
+            ...element,
+            color : colors[element.category]
+        };
+    }
+);
+
+// console.log(colorIcons);
+
+printIcons(colorIcons,containerIcons);
+
+const iconCategories = [];
+
+icons.forEach(
+    (element) =>{
+        if (iconCategories.includes(element.category) == false){
+            iconCategories.push(element.category);
+        }
+
+    }
+);
+console.log(iconCategories);
+
+const selectedCategories = document.getElementById("category");
+
+iconCategories.forEach(
+    (element) =>{
+        selectedCategories.innerHTML += `<option value="${element}">${element}</option>`
+    }
+);
+
+// evento change
+
+selectedCategories.addEventListener("change" ,
+    function(){
+        const iconsFiltered = colorIcons.filter(
+            (element) =>{
+                if (element.category == selectedCategories.value || selectedCategories.value == ""){
+                    return true;
+                }
+                return false;
+            }
+        
+        )
+        printIcons(iconsFiltered,containerIcons);
     }
 );
